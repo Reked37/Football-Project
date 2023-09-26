87b59c3a2fc7bb11e0d02f9c47602a00bc60de34
Derek Truesdale <dtruesdale37@gmail.com>
2023-09-26 14:27:49 -0500

    created some seed data

--- server/seed.py
+++ server/seed.py
@@ -13,5 +13,44 @@ from models import db
 if __name__ == '__main__':
     fake = Faker()
     with app.app_context():
+        teams=['Chargers', 'Cowboys', 'Jets', 'Vikings', 'Browns', 'Cardinals', 'Panthers']
+        def delete_tables():
+            session.query(Player).delete()
+            session.query(Team).delete()
+            session.query(City).delete()
+            session.commit()
         print("Starting seed...")
-        # Seed code goes here!
+        
+        def create_players():
+            players=[]
+            for player in range(10):
+                player=Player(
+                    name=fake.name(),
+                    jersey_number=fake.number(),
+                    team=rc(teams)
+                )
+                session.add(player)
+                session.commit()
+                players.append(player)
+
+        def create_teams():
+            for team in range(10):
+                team=Team(
+                    name=fake.name(),
+                    mascot=fake.name()
+                )
+                session.add(team)
+                session.commit()
+                
+        def create_city():
+            for city in range(10):
+                city=City(
+                    name=fake.name(),
+                    population=fake.number()
+                )
+                session.add(city)
+                session.commit()
+
+
+
+        
