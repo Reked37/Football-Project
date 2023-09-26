f1224d4ba2c4de9e7b8e4db1e84b3b722c920f74
Derek Truesdale <dtruesdale37@gmail.com>
2023-09-26 12:46:12 -0500

    created tables

--- server/models.py
+++ server/models.py
@@ -1,6 +1,33 @@
+from sqlalchemy import MetaData, ForeignKey
+from sqlalchemy.orm import validates, relationship
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
 
 from config import db
 
 # Models go here!
+class Player(db.Model, SerializerMixin):
+    __tablename__ = 'players'
+
+    id= db.Column(db.Integer, primary_key=True)
+    name= db.Column(db.String, nullable=False)
+    number= db.Column(db.Integer)
+
+    team=db.relationship()
+
+    serialize_rules=()
+
+class Team(db.Model, SerializerMixin):
+    __tablename__= 'teams'
+
+    id=db.Column(db.Integer, primary_key=True)
+    name=db.Column(db.String, nullable=False)
+    mascot=db.Column(db.String)
+
+class City(db.Model, SerializerMixin):
+    __tablename__= 'cities'
+
+    id=db.Column(db.Integer, primary_key=True)
+    name=db.Column(db.String, nullable=False, unique=True)
+    population=db.Column(db.Integer)
+
