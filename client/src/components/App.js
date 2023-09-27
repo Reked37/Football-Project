0ab611dc403eb0b874c01e1ce2a70645b1a247de
Derek Truesdale <dtruesdale37@gmail.com>
2023-09-27 12:17:40 -0500

    working on frontend

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,15 +1,41 @@
 import React, { useEffect, useState } from "react";
-import { Routes, Route } from "react-router-dom";
+import {BrowserRouter as Routes, Route } from "react-router-dom";
+import Home from "./Home";
 
 function App() {
-  const [players, setPlayer]=[]
+  const [players, setPlayers]=useState([])
+  const [coaches, setCoaches]=useState([])
+  const [teams, setTeams]=useState([])
 
   useEffect(()=>{
     fetch('http://localhost:5555/players')
-  })
+    .then(res=>res.json())
+    .then(data=>setPlayers(data),
+  )},[])
 
+  useEffect(()=>{
+    fetch('http://localhost:5555/teams')
+    .then(res=>res.json())
+    .then(data=>setTeams(data),
+  )},[])
 
-  return <h1>Project Client</h1>;
+  useEffect(()=>{
+    fetch('http://localhost:5555/coaches')
+    .then(res=>res.json())
+    .then(data=>setCoaches(data),
+  )},[])
+
+  console.log('players:', players)
+  console.log('teams:', teams)
+  console.log('coaches:', coaches)
+
+  return(
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes> 
+    </div>
+  );
 }
 
 export default App;
